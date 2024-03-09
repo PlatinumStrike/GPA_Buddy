a3e1df30b7e03d6bae0f0bb861426309fae6b3d4
Justin Hanna <hanna.jh1@gmail.com>
2024-03-09 01:25:50 -0500

    Create basic HTML layout and page switcher

---
+++ src/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="/css/output.css">
+    <link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura.css" type="text/css">
+    <base href="/">
+</head>
+
+<body class="text-center">
+    <?php
+    switch (explode("?", $_SERVER['REQUEST_URI'])[0]) {
+        case '/login':
+            require 'inc/login.php';
+            include 'tpl/login.tpl.php';
+            break;
+
+        case '/signup':
+            require 'inc/signup.php';
+            include 'tpl/signup.tpl.php';
+            break;
+
+        case '/home':
+        case '/':
+            include 'tpl/home.tpl.php';
+            break;
+
+        default:
+            header('HTTP/1.0 404 Not Found');
+            include 'tpl/page_not_found.tpl.php';
+            break;
+    }
+    ?>
+</body>
+
+</html>
