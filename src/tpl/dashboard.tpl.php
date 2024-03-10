3731a20ba817860a9dc4d922a13d536cdb3c1072
Justin Hanna <hanna.jh1@gmail.com>
2024-03-10 16:34:31 -0400

    update dashbord UI

--- src/tpl/dashboard.tpl.php
+++ src/tpl/dashboard.tpl.php
@@ -1,15 +1,22 @@
 <h1>Dashboard</h1>
 
-<form action="/inc/dashboard.php" method="POST" enctype="multipart/form-data" class="my-20 px-8 py-4 border-2">
-    <input type="hidden" name="form_title" value="upload_transcript">
-    <h2 class="mt-6">Upload Transcript</h2>
-    <p>Open <a href="https://mosaic.mcmaster.ca/">MOSAIC</a>, go to Grades, My Academics, View my Course History, then copy and paste the table presented into here:</p>
-    <textarea name="incoming_transcript" id="incomingTranscript"></textarea>
-    <br>
-    <input type="submit" value="Upload Transcript">
-</form>
+<div class="my-20 px-8 py-4 border-2 collapsible">
+    <div class="collapsible-header">
+        <h2 class="mt-6">Upload Transcript</h2>
+    </div>
+    <div class="collapsible-body">
+        <p>Open <a href="https://mosaic.mcmaster.ca/">MOSAIC</a>, go to Grades, My Academics, View my Course History, then copy and paste the grade table below</p>
+        <form action="/inc/dashboard.php" method="POST" enctype="multipart/form-data">
+            <input type="hidden" name="form_title" value="upload_transcript">
+            <label for="incomingTranscript">Paste Transcript here</label>
+            <textarea name="incoming_transcript" id="incomingTranscript"></textarea>
+            <br>
+            <input type="submit" value="Upload Transcript">
+        </form>
+    </div>
+</div>
 
-<?= $transcript_table ?? null ?>
+<?= $course_list ?? "Please upload a transcript" ?>
 <br>
 
 <a href="/">Go Home</a>
