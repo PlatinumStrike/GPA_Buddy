668aab59f9bab53d4ea5075b0f7ed7e9fe95f072
Justin Hanna <hanna.jh1@gmail.com>
2024-03-10 04:08:52 -0400

    update data input process

--- src/inc/dashboard.php
+++ src/inc/dashboard.php
@@ -1,4 +1,12 @@
 <?php
+
+require_once(dirname(__DIR__, 1) . "/config.php");
+require_once("database.php");
+
+$transcript_data = "";
+
+require("navigation.php");
+
 if (!isset($_SESSION['user_id'])) {
     echo "<h1>Dashboard</h1>" .
         "<p>You are not currently logged in.</p>" .
@@ -6,5 +14,32 @@ if (!isset($_SESSION['user_id'])) {
         "<a href='/login'><button>Login</button></a></div>";
     exit();
 }
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $transcript_data = json_decode(Database::selectQuery("SELECT transcript from transcripts where id=?", [$_SESSION['user_id']])['transcript']);
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    switch ($_POST['form_title']) {
+        case "upload_transcript":
+            $table = explode("\n", $_POST['incoming_transcript']);
+            $header = explode("\t", $table[0]);
+            array_shift($table);
+            $data = array_chunk($table, 6);
+            $data = array_map(function ($obj) {
+                return [
+                    "course" => $obj[0],
+                    "description" => $obj[1],
+                    "term" => $obj[2],
+                    "grade" => $obj[3],
+                    "units" => $obj[4],
+                    "status" => $obj[5],
+                ];
+            }, $data);
+            Database::insertQuery("INSERT INTO transcripts (id, transcript) VALUES (?,?) ON DUPLICATE KEY UPDATE transcript=?", [$_SESSION['user_id'], json_encode($data), json_encode($data)]);
+            redirect("/dashboard");
+        default:
+            break;
+    }
+}
+
 
 // EOF
--- src/tpl/dashboard.tpl.php
+++ src/tpl/dashboard.tpl.php
@@ -1,5 +1,15 @@
 <h1>Dashboard</h1>
 
+<form action="/inc/dashboard.php" method="POST" enctype="multipart/form-data" class="my-20 px-8 py-4 border-2">
+    <input type="hidden" name="form_title" value="upload_transcript">
+    <h2 class="mt-6">Upload Transcript</h2>
+    <p>Open <a href="https://mosaic.mcmaster.ca/">MOSAIC</a>, go to Grades, My Academics, View my Course History, then copy and paste the table presented into here:</p>
+    <textarea name="incoming_transcript" id="incomingTranscript"></textarea>
+    <br>
+    <input type="submit" value="Upload Transcript">
+</form>
+
+<?= $transcript_table ?? null ?>
 <br>
 
 <a href="/">Go Home</a>
