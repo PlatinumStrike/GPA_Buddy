9275465a3b4000543f82ac36b4c77c38c733595f
Justin Hanna <hanna.jh1@gmail.com>
2024-03-11 03:06:05 -0400

    fix up homepage UI

--- src/inc/home.php
+++ src/inc/home.php
@@ -1,19 +1,21 @@
 <?php
 
-$getStarted = "<h2>Get Started Today!</h2>" .
+$getStarted = "<div class='pb-8 pt-4 rounded-xl bg-gray-200'><h2 class='mt-2'>Get Started Today!</h2>" .
     "<div><a href='/signup'><button>Sign Up</button></a> " .
-    "<a href='/login'><button>Login</button></a></div>";
+    "<a href='/login'><button>Login</button></a></div></div>";
 
 if (isset($_POST['logout'])) {
     session_unset();
 }
 
+$title_class = "";
 if (isset($_SESSION['user_id'])) {
-    $getStarted = "<a href='/dashboard'><button>Open Dashboard</button></a> ";
+    $getStarted = "<a href='/dashboard'><button class='px-8 py-2 rounded-xl'>Open Dashboard</button></a> ";
     echo "<div class='flex flex-row justify-between'>" .
         "<p>Signed in to " . $_SESSION['user_email'] . "</p>" .
         "<form action='/' method='post'><input type='submit' name='logout' value='Log Out'></input></form>" .
         "</div>";
+    $title_class = "mt-2";
 }
 
 // EOF
--- src/tpl/home.tpl.php
+++ src/tpl/home.tpl.php
@@ -1,20 +1,22 @@
-<h1>GPA Buddy</h1>
+<h1 class=<?= $title_class ?>>GPA Buddy</h1>
 
 <p>Welcome to GPA Buddy, your first step to becoming an academic weapon!</p>
 
 <br>
 
-<div class="grid grid-flow-row grid-cols-2 place-items-center">
+<div class="b-gray-200 border-2 rounded-2xl p-2">
+    <div class="grid grid-flow-row grid-cols-2 place-items-center ">
 
-    <p>Just Login,</p>
-    <img src="/imgs/login.png" alt="Image of login page">
+        <p>Just Login,</p>
+        <img src="/imgs/login.png" alt="Image of login page">
 
-    <object data="/imgs/file-upload-line.svg" alt="Image of upload page"></object>
-    <p>Upload Your Transcript,</p>
+        <p>Upload Your Transcript,</p>
+        <img src="/imgs/transcript.png" alt="Image of dashboard page">
+        <!-- <object data="/imgs/file-upload-line.svg" alt="Image of upload page"></object> -->
 
+    </div>
+    <p>And that's it!</p>
 </div>
-
-<p>And that's it!</p>
 
 <br>
 
